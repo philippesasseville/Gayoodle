122e9713e29cffcd309b015b50de575ab4e02959
Philippe Sasseville <philippe.sasseville@polymtl.ca>
2016-09-30 16:20:01 -0400

    pug pug nigger

--- TP2/myApp/routes/index.js
+++ TP2/myApp/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'INDEX' });
+  res.render('index', { title: 'QuizFacile', path: req.path });
+});
+
+router.get('/game', function(req, res) {
+  res.render('game', { title: 'QuizFacile',  path: req.path });
+});
+
+router.get('/rules', function(req, res) {
+  res.render('rules', { title: 'QuizFacile', path: req.path  });
 });
 
 module.exports = router;
