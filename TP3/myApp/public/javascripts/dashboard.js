55c8e969d81d656e223e7e8a15877be72c3fed7a
Tremblex <alex.tremblay9@gmail.com>
2016-10-26 19:57:46 -0400

    pls

--- TP3/myApp/public/javascripts/dashboard.js
+++ TP3/myApp/public/javascripts/dashboard.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
 	$("#id_button_exam").click(function() {
 		sessionStorage.setItem("theme", $('select[name=domaine] option:selected').attr('value'));
 		sessionStorage.setItem("nbquestions", $('select[name=nbquestions] option:selected').attr('value'));
-		window.location.href = '/exam'
+		location.href = '/exam';
 	})
 
 });
