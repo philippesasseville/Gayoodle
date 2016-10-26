7bb0b303a765911922c0f04a44bf8a8e74daeab1
Philippe Sasseville <philippe.sasseville@polymtl.ca>
2016-10-26 14:15:54 -0400

    dnd + note courante

--- TP3/myApp/public/javascripts/quicktest.js
+++ TP3/myApp/public/javascripts/quicktest.js
@@ -1,67 +1,58 @@
 var completedQuestions;
+var goodAnswer;
+var q = 0;
+var qr = 0;
 
 $(document).ready(function() {
-  var url = "./api/randomQuestion";
-  var currentQuestion;
-     
-	// resetDB();
-	//initQuestionHistory();
-  
 
-	var strCompletedQuestions = JSON.stringify(completedQuestions);
-	var query = "q="+strCompletedQuestions;
-	//TODO: ADD COMPLETED TO QUERY FOR SERVER
-
-	$.getJSON(url, /*,{"q": strCompletedQuestions},*/ function(data) {
-		currentQuestion = data.id;
-		$("#id_title").text(data.theme);
-		$("#id_question").text(data.question);
-		$("#id_ans1").text(data.rep1.text);
-		$("#id_ans2").text(data.rep2.text);
-		$("#id_ans3").text(data.rep3.text);
-    $("#col_ans1 p").text(data.rep1.text);
-    $("#col_ans2 p").text(data.rep2.text);
-    $("#col_ans3 p").text(data.rep3.text);
-	});
+	nextQuestion();
 
 	$("#id_button_next").click(function() {
-		if (!arrayContains(currentQuestion)) {
-			completedQuestions.push(currentQuestion);
-			localStorage.setItem("completedQuestions", JSON.stringify(completedQuestions));
-		}
-		location.href='/quicktest';
+		console.log($('#ans p').text());
+		updateNote(checkAnswer($('#ans p').text()));
+		nextQuestion();
 	})
 
 });
 //borken document ready lel
 var updateNote = function(ans) {
-	var q = sessionStorage.getItem('nbQuestion');
-	var qr = sessionStorage.getItem('nbQuestionReussi');
 	if(ans === true){
-		sessionStorage.setItem('nbQuestionReussi', qr++);
+		qr++;
 	}
-	else{
-		sessionStorage.setItem('nbQuestionReussi', qr);
-	}
-	sessionStorage.setItem('nbQuestion', q++);
-	$("#note").text("Note Courante : "+qr/q+"%");
+	q++;
+	var note = ((qr/q)*100);
+	note = note.toFixed(0);
+	$("#note").text("Note Courante : "+note+"%");
 };
 
-var initQuestionHistory = function() {
-	completedQuestions = JSON.parse(localStorage.getItem("completedQuestions"));
-	if (completedQuestions == null) {
-		completedQuestions = [];
-	}
-	//console.log(completedQuestions);
+var nextQuestion = function(){
+	var url = "./api/randomQuestion";
+	$.getJSON(url, function(data) {
+		$("#id_title").text(data.theme);
+		$("#id_question").text(data.question);
+		$("#col_ans1 p").text(data.rep1.text);
+		$("#col_ans2 p").text(data.rep2.text);
+		$("#col_ans3 p").text(data.rep3.text);
+		if(data.rep1.ans == true){
+			goodAnswer = data.rep1.text;
+		}else if (data.rep2.ans == true){
+			goodAnswer = data.rep2.text;
+		}else{
+			goodAnswer = data.rep3.text;
+		}
+	});
+	var cols = document.querySelectorAll('#columns .column');
+	[].forEach.call(cols, function(col) {
+	  $(col).attr("draggable","true");
+	  $(col).removeClass("chosen");
+	});
+	$('#ans p').text("Glisser votre reponse ici");
+
 };
 
-var arrayContains = function(data) {
-	var found = false;
-	for(var i = 0; i < completedQuestions.length; i++) {
-	    if (completedQuestions[i] == data) {
-	        found = true;
-	        break;
-	    }
-	}
-	return found;
+var checkAnswer = function(str){
+	console.log("check ans");
+	console.log(goodAnswer);
+	console.log(str);
+	return str == goodAnswer;
 };
