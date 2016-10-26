66830b45d1659df202185b4e564c05e85717b238
Philippe Sasseville <philippe.sasseville@polymtl.ca>
2016-10-26 15:53:43 -0400

    move code etc

--- TP3/myApp/public/javascripts/testUtil.js
+++ TP3/myApp/public/javascripts/testUtil.js
@@ -27,7 +27,8 @@ var nextQuestion = function(){
 	var cols = document.querySelectorAll('#columns .column');
 	[].forEach.call(cols, function(col) {
 	  $(col).attr("draggable","true");
-	  $(col).removeClass("chosen");
+	  $(col).removeClass("good");
+	  $(col).removeClass("bad");
 	});
 	$('#ans p').text("Glisser votre reponse ici");
 
@@ -54,3 +55,82 @@ var setGoodAnswer = function(data) {
 			goodAnswer = data.reponses[2].text;
 		}
 }
+
+///////////////////////////////////////////////////////
+
+
+var dragSrcEl = null;
+
+function handleDragStart(e) {
+  // Target (this) element is the source node.
+  //this.style.opacity = '0.4';
+
+  dragSrcEl = this;
+
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', $(this).find("p").text());
+}
+
+function handleDrop(e) {
+  // this/e.target is current target element.
+  if (e.stopPropagation) {
+    e.stopPropagation(); // Stops some browsers from redirecting.
+  }
+
+  // on verifie que la drop zone est bonne
+  if ($(this).attr('id') === "ans") {
+	// on popule le tag p
+    $(this).find("p").text(e.dataTransfer.getData('text/html'));
+    // classe pour lindicateur visuel
+    if($(this).find("p").text() == goodAnswer){
+		$(dragSrcEl).addClass("good");
+	}
+	else{
+		$(dragSrcEl).addClass("bad");
+	}
+	// on desactive le drag car le user ne peux pas changer de reponse.
+    [].forEach.call(cols, function(col) {
+	  $(col).attr("draggable","false");
+	});
+  }
+
+  return false;
+}
+
+function handleDragOver(e) {
+  if (e.preventDefault) {
+    e.preventDefault(); // Necessary. Allows us to drop.
+  }
+
+  e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+
+  return false;
+}
+
+function handleDragEnter(e) {
+  // this / e.target is the current hover target.
+  this.classList.add('over');
+}
+
+function handleDragLeave(e) {
+  this.classList.remove('over');  // this / e.target is previous target element.
+}
+
+function handleDragEnd(e) {
+  // this/e.target is the source node.
+
+  [].forEach.call(cols, function (col) {
+    col.classList.remove('over');
+  });
+}
+
+// on lie les fonction au evenement du modele drag and drop html5
+var cols = document.querySelectorAll('#columns .column');
+[].forEach.call(cols, function(col) {
+  col.addEventListener('dragstart', handleDragStart, false);
+  col.addEventListener('dragenter', handleDragEnter, false)
+  col.addEventListener('dragover', handleDragOver, false);
+  col.addEventListener('dragleave', handleDragLeave, false);
+  col.addEventListener('drop', handleDrop, false);
+  col.addEventListener('dragend', handleDragEnd, false);
+});
